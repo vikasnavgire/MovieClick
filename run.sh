b4b4f036a5f35e1ad9355959d42eeb3d5e2528e8
Vikas N <viki.81188@gmail.com>
2017-07-25 15:35:33 +0530

    added run.sh for docker up

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+## This is docker application run automated script. - Vikas N, July 2017 
+
 set -x # Debug mode
 
 docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=db -e MYSQL_USER=root -p 3306:3306 -d mysql
+
 docker run -d -p 5000:5000 -v /home/vikas/workspace/MovieClick/:/app/ --name movie-click movieclick
